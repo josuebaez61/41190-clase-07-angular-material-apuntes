40c66b32839d3735043f3f42b9ab5d3c90eb4a27
josue baez <josuebaez61@gmail.com>
2023-04-02 10:49:34 -0300

    ADD, angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutsModule } from './layouts/layouts.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layouts/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environments';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  isProd = environment.isProduction;
+}
